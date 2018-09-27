ef5cdeebdea28dbdc81848e90167a9fb6804e414
leandrocgsi <leandrocgsi@gmail.com>
2018-09-27 15:35:40 -0300

    Adding JUnit support

--- 00 - Scaffold/src/main/java/br/com/erudio/Startup.java
+++ 00 - Scaffold/src/main/java/br/com/erudio/Startup.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPlaygroundApplication {
+public class Startup {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPlaygroundApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+    }
 }
