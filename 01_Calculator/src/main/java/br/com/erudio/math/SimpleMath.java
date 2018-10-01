46b7176762fc04025f69790a668024c1cb033aac
Leandro Costa <leandrocgsi@gmail.com>
2018-10-01 15:26:36 -0300

    Update SimpleMath.java

--- 01_Calculator/src/main/java/br/com/erudio/math/SimpleMath.java
+++ 01_Calculator/src/main/java/br/com/erudio/math/SimpleMath.java
@@ -2,27 +2,27 @@ package br.com.erudio.math;
 
 public class SimpleMath {
 
-    public Long Sum(Long firstNumber, Long secondNumber) {
+    public Long sum(Long firstNumber, Long secondNumber) {
         return firstNumber + secondNumber;
     }
 
-    public Long Subtraction(Long firstNumber, Long secondNumber) {
+    public Long subtraction(Long firstNumber, Long secondNumber) {
         return firstNumber - secondNumber;
     }
 
-    public Long Multiplication(Long firstNumber, Long secondNumber) {
+    public Long multiplication(Long firstNumber, Long secondNumber) {
         return firstNumber * secondNumber;
     }
 
-    public Long Division(Long firstNumber, Long secondNumber) {
+    public Long division(Long firstNumber, Long secondNumber) {
         return firstNumber / secondNumber;
     }
 
-    public Long Mean(Long firstNumber, Long secondNumber) {
+    public Long mean(Long firstNumber, Long secondNumber) {
         return (firstNumber + secondNumber) / 2;
     }
     
-    public Float SquareRoot(Long number) {
+    public Float squareRoot(Long number) {
         return (float) Math.sqrt(number);
     }
 }
