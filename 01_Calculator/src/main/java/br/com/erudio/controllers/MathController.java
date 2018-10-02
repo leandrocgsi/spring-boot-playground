57ab91577bf2648072abc9e0b58afcfd5db5aa26
leandrocgsi <leandrocgsi@gmail.com>
2018-10-02 14:49:24 -0300

    Implementing simple six math operations

--- 01_Calculator/src/main/java/br/com/erudio/controllers/MathController.java
+++ 01_Calculator/src/main/java/br/com/erudio/controllers/MathController.java
@@ -24,6 +24,61 @@ public class MathController {
         Float sum = math.sum(CovertToDecimal(numberOne), CovertToDecimal(numberTwo));
         return sum;
     }
+    
+    @RequestMapping(value="/subtraction/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Float subtraction(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        
+        if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)) {
+            throw new MathOperationUnsuportedException("Please set a numeric value");
+        }
+        
+        Float subtraction = math.subtraction(CovertToDecimal(numberOne), CovertToDecimal(numberTwo));
+        return subtraction;
+    }
+    
+    @RequestMapping(value="/division/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Float division(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        
+        if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)) {
+            throw new MathOperationUnsuportedException("Please set a numeric value");
+        }
+        
+        Float division = math.division(CovertToDecimal(numberOne), CovertToDecimal(numberTwo));
+        return division;
+    }
+    
+    @RequestMapping(value="/multiplication/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Float multiplication(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        
+        if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)) {
+            throw new MathOperationUnsuportedException("Please set a numeric value");
+        }
+        
+        Float multiplication = math.multiplication(CovertToDecimal(numberOne), CovertToDecimal(numberTwo));
+        return multiplication;
+    }
+    
+    @RequestMapping(value="/mean/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+    public Float mean(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        
+        if (!IsNumeric(numberOne) || !IsNumeric(numberTwo)) {
+            throw new MathOperationUnsuportedException("Please set a numeric value");
+        }
+        
+        Float mean = math.mean(CovertToDecimal(numberOne), CovertToDecimal(numberTwo));
+        return mean;
+    }
+
+    @RequestMapping(value="/squareRoot/{number}", method=RequestMethod.GET)
+    public Float squareRoot(@PathVariable("number") String number) {
+        
+        if (!IsNumeric(number)) {
+            throw new MathOperationUnsuportedException("Please set a numeric value");
+        }
+        
+        Float squareRoot = math.squareRoot(CovertToDecimal(number));
+        return squareRoot;
+    }
 
     private Float CovertToDecimal(String number) {
         if (IsNumeric(number)) return Float.parseFloat(number);
