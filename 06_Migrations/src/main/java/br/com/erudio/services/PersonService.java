2237edf3d1f102ec4ac17466cfcb99e4b253e5c8
leandrocgsi <leandrocgsi@gmail.com>
2019-03-31 19:07:38 -0300

    Working with migrations

---
+++ 06_Migrations/src/main/java/br/com/erudio/services/PersonService.java
@@ -0,0 +1,54 @@
+package br.com.erudio.services;
+ 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.data.models.Person;
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.exception.ResourceNotFoundException;
+import br.com.erudio.repository.PersonRepository;
+ 
+@Service
+public class PersonService {
+     
+    @Autowired
+    PersonRepository repository;
+    
+    public PersonVO create(PersonVO person) {
+    	var entity = DozerConverter.parseObject(person, Person.class);
+    	var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+    	return vo;
+    }
+ 
+    public PersonVO findById(Long id) {
+    	var entity = repository.findById(id)
+        		.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        return DozerConverter.parseObject(entity, PersonVO.class);
+    }
+ 
+    public List<PersonVO> findAll() {
+    	return DozerConverter.parserListObjects(repository.findAll(), PersonVO.class);
+    }
+     
+    public PersonVO update(PersonVO person) {
+    	Person entity = repository.findById(person.getId())
+                 .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+		entity.setAddress(person.getAddress());
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setGender(person.getGender());
+		var vo = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+    	return vo;
+    }
+ 
+    public void delete(Long id) {
+    	Person person = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		repository.delete(person);
+    }
+}
