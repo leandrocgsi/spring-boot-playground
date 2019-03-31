eb13844fe341cf7f0f652e47b90f638e6df2e983
leandrocgsi <leandrocgsi@gmail.com>
2019-03-31 19:52:29 -0300

    Working with migrations

---
+++ 06_Migrations/src/main/resources/db/migration/V2__PopulatePerson.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Patos de Minas - Minas Gerais - Brasil', 'Flávio', 'Male', 'Costa'),
+	(2, 'Uberlândia - Minas Gerais - Brasil', 'Leandro', 'Male', 'Costa'),
+	(5, 'Bento Gonçalves - Rio Grande do Sul - Brasil', 'Alan', 'Male', 'Santos');
