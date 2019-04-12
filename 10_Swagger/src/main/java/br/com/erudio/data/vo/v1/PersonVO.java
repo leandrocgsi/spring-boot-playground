f2f4bcdee69dc11d7115b34d6204a231cf23137f
leandrocgsi <leandrocgsi@gmail.com>
2019-04-11 23:52:03 -0300

    Adding Swagger support

--- 10_Swagger/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ 10_Swagger/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.springframework.hateoas.ResourceSupport;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.dozermapper.core.Mapping;
  
 public class PersonVO extends ResourceSupport implements Serializable{
@@ -11,6 +12,7 @@ public class PersonVO extends ResourceSupport implements Serializable{
     private static final long serialVersionUID = 1L;
      
     @Mapping("id")
+    @JsonProperty("id")
     private Long key;
     private String firstName;
     private String lastName;
