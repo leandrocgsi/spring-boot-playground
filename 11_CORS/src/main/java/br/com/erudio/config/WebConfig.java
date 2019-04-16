9db88b1e26d7be7a0d2ab923a7a1f8861ac41df9
leandrocgsi <leandrocgsi@gmail.com>
2019-04-15 23:57:19 -0300

    Working with CORS

--- 11_CORS/src/main/java/br/com/erudio/config/WebConfig.java
+++ 11_CORS/src/main/java/br/com/erudio/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+		.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	}
 
 	@Override
