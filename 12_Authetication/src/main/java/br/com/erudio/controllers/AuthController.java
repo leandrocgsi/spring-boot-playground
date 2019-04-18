dbfb1480feac13b1c9e4acea3fe50ec37f9a87b8
leandrocgsi <leandrocgsi@gmail.com>
2019-04-18 12:17:58 -0300

    Authenticatin and authorization

---
+++ 12_Authetication/src/main/java/br/com/erudio/controllers/AuthController.java
@@ -0,0 +1,66 @@
+package br.com.erudio.controllers;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.repository.UserRepository;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+import br.com.erudio.security.vo.AccountCredentialsVO;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenProvider tokenProvider;
+    
+    @Autowired
+    UserRepository repository;
+
+    @SuppressWarnings("rawtypes")
+	@RequestMapping(method = RequestMethod.POST, value = "/signin", consumes = { "application/json", "application/xml", "application/x-yaml" },
+    	    produces = { "application/json", "application/xml", "application/x-yaml" })
+    public ResponseEntity signin(@RequestBody AccountCredentialsVO data) {
+
+        try {
+            var username = data.getUsername();
+            var password = data.getPassword();
+            
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+            var user = this.repository.findByUsername(username);
+            var token = "";
+            
+            if (user != null) {
+                token = tokenProvider.createToken(username, user.getRoles());
+            } else {
+                throw new UsernameNotFoundException("Username " + username + "not found");
+            }
+
+            Map<Object, Object> model = new HashMap<>();
+            model.put("username", username);
+            model.put("token", token);
+            
+            return ok(model);
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+    }
+}
--- 12_Authetication/src/main/java/br/com/erudio/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ 12_Authetication/src/main/java/br/com/erudio/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.erudio.exception.ExceptionResponse;
+import br.com.erudio.exception.InvalidJwtAuthenticationException;
 import br.com.erudio.exception.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -37,5 +38,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 						request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+    public final ResponseEntity<ExceptionResponse> invalidJwtAuthentication(InvalidJwtAuthenticationException ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = 
+				new ExceptionResponse(
+						new Date(),
+						ex.getMessage(),
+						request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.UNAUTHORIZED);
+    }
 
 }
