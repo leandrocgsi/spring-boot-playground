87791bed80958fa781838783db8d28c90f610ef9
leandrocgsi <leandrocgsi@gmail.com>
2019-05-12 19:29:25 -0300

    Implementing pagination search

--- 14_PaginationSearch/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 14_PaginationSearch/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.data.models.Person;
 import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.services.PersonService;
 import io.swagger.annotations.Api;
