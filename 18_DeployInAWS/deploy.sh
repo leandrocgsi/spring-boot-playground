89ab17533aa57cb08e87a506a41fed570f91dd55
Leandro Costa <leandrocgsi@gmail.com>
2019-07-09 12:57:16 -0300

    Update deploy.sh

--- 18_DeployInAWS/deploy.sh
+++ 18_DeployInAWS/deploy.sh
@@ -25,5 +25,6 @@ echo "Deploy to AWS are Started!"
 # docker push $IMAGE_REPO_URL:latest
 
 # Push to Amazon AWS ECS
-echo "ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest"
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest 
+# echo "ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest"
+# ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest 
+ecs-deploy -d $TASK_DEFINITION_NAME -i $IMAGE_REPO_URL:latest
