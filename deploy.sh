3910145f9c25370459f170c7ea7789a7487fefff
leandrocgsi <leandrocgsi@gmail.com>
2019-07-08 21:57:01 -0300

    Deploy to Amazon AWS

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin
+
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+# Build Maven Project
+
+echo "Let's start Maven Package!"
+mvn clean package
+cd ..
+echo "We are in the Docker folder!"
+# Starting Docker Compose Build!"
+
+docker-compose build
+
+# Use this for AWS ECR
+# eval $(aws ecr get-login --region us-east-1)
+
+# Push to Docker Hub
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+docker push leandrocgsi/rest-with-spring-boot-udemy
+
+# Push to Amazon AWS ECR
+eval $(aws ecr get-login --no-include-email --region us-east-2)
+docker tag leandrocgsi/rest-with-spring-boot-udemy:latest $IMAGE_REPO_URL:latest
+echo "Deploy to AWS are Started!" 
+docker push $IMAGE_REPO_URL:latest
+
+# Push to Amazon AWS ECS
+ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest 
