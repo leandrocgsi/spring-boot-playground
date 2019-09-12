511daddd4dc510d9d4768cf855a860fea9ed09d8
leandrocgsi <leandrocgsi@gmail.com>
2019-09-11 21:48:38 -0300

    Working with Mongo DB

--- 19_MongoDB/src/main/java/br/com/erudio/Application.java
+++ 19_MongoDB/src/main/java/br/com/erudio/Application.java
@@ -25,6 +25,7 @@ public class Application {
     
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
     
     @Bean
     public SwaggerSpringMvcPlugin groupOnePlugin() {
