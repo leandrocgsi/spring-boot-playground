ee75701c50412da9a88c2d77f60e6bbf415d7f07
leandrocgsi <leandrocgsi@gmail.com>
2020-03-23 13:07:28 -0300

    Updating frameworks versions

--- 20_MigratingToNewVersions/src/main/java/br/com/erudio/controller/BookController.java
+++ 20_MigratingToNewVersions/src/main/java/br/com/erudio/controller/BookController.java
@@ -1,16 +1,17 @@
 package br.com.erudio.controller;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PagedResourcesAssembler;
-import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -38,7 +39,7 @@ public class BookController {
 	
 	@Autowired
 	private PagedResourcesAssembler<BookVO> assembler;
-	
+		
 	@ApiOperation(value = "Find all books" )
 	@GetMapping(produces = { "application/json", "application/xml", "application/x-yaml" })
 	public ResponseEntity<?> findAll(
@@ -47,6 +48,8 @@ public class BookController {
 			@RequestParam(value="direction", defaultValue = "asc") String direction) {
 		
 
+		var controller = methodOn(BookController.class);
+		
 		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 		
 		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "title"));
@@ -55,20 +58,22 @@ public class BookController {
 		books
 			.stream()
 			.forEach(p -> p.add(
-					linkTo(methodOn(BookController.class).findById(p.getKey())).withSelfRel()
+					linkTo(controller.findById(p.getKey())).withSelfRel()
 				)
 			);
 		
-		PagedResources<?> resources = assembler.toResource(books);
+		PagedModel<?> resources = assembler.toResource(books);
 		
 		return new ResponseEntity<>(resources, HttpStatus.OK);
 	}	
 	
 	@ApiOperation(value = "Find a specific book by your ID" )
 	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yaml" })
-	public BookVO findById(@PathVariable("id") Long id) {
+	public Resource<BookVO> findById(@PathVariable("id") Long id) {
+
+		var controller = methodOn(BookController.class);
 		BookVO bookVO = service.findById(id);
-		bookVO.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
+		bookVO.add(linkTo(controller.findById(id)).withSelfRel());
 		return bookVO;
 	}	
 	
