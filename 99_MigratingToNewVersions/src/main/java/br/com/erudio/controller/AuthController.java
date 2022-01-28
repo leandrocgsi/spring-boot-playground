f822cd123055376fce5a967764ac6674103c8adf
leandrocgsi <leandrocgsi@gmail.com>
2022-01-28 09:46:09 -0300

    Migrating to Java 17 and Spring Boot 2.6.3

--- 99_MigratingToNewVersions/src/main/java/br/com/erudio/controller/AuthController.java
+++ 99_MigratingToNewVersions/src/main/java/br/com/erudio/controller/AuthController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.erudio.security.AccountCredentialsVO;
+import br.com.erudio.data.vo.v1.security.AccountCredentialsVO;
 import br.com.erudio.services.AuthServices;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
