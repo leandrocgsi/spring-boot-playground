eab01d6fc24842b158f1e1e18d706a29b0d4e77a
leandrocgsi <leandrocgsi@gmail.com>
2022-01-27 23:59:13 -0300

    Migrating to Java 17 and Spring Boot 3.0.0-M1

--- 99_MigratingToNewVersions/src/main/java/br/com/erudio/config/SecurityConfig.java
+++ 99_MigratingToNewVersions/src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package br.com.erudio.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,7 +10,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 import br.com.erudio.security.jwt.JwtConfigurer;
 import br.com.erudio.security.jwt.JwtTokenProvider;
@@ -18,10 +23,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private JwtTokenProvider tokenProvider;
 	
+	/*
+	 * @Bean public BCryptPasswordEncoder passwordEncoder() { BCryptPasswordEncoder
+	 * bCryptPasswordEncoder = new BCryptPasswordEncoder(); return
+	 * bCryptPasswordEncoder; }
+	 */
+	
 	@Bean
-	public BCryptPasswordEncoder passwordEncoder() {
-		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-		return bCryptPasswordEncoder;
+	public PasswordEncoder passwordEncoder() {
+		/*
+		 * PasswordEncoder encoder =
+		 * PasswordEncoderFactories.createDelegatingPasswordEncoder(); Map<String,
+		 * PasswordEncoder> encoders = new HashMap<>(); encoders.put("bcrypt", new
+		 * BCryptPasswordEncoder());
+		 * 
+		 * DelegatingPasswordEncoder passworEncoder = new DelegatingPasswordEncoder(
+		 * "bcrypt", encoders);
+		 * passworEncoder.setDefaultPasswordEncoderForMatches(encoder);
+		 */
+
+	    //return passworEncoder;
+	    //return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		Map<String, PasswordEncoder> encoders = new HashMap<>();
+		encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder());
+        return passwordEncoder;
 	}
 	
 	@Bean
