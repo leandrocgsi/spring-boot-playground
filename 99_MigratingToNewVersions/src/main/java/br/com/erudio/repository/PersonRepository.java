acfeb47d1683a607ae4691dd5a8d51765f72b359
leandrocgsi <leandrocgsi@gmail.com>
2022-01-28 09:47:06 -0300

    Migrating to Java 17 and Spring Boot 2.6.3

--- 99_MigratingToNewVersions/src/main/java/br/com/erudio/repository/PersonRepository.java
+++ 99_MigratingToNewVersions/src/main/java/br/com/erudio/repository/PersonRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import br.com.erudio.data.model.Person;
+import br.com.erudio.model.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long>{
--- 99_MigratingToNewVersions/src/main/java/br/com/erudio/repository/UserRepository.java
+++ 99_MigratingToNewVersions/src/main/java/br/com/erudio/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import br.com.erudio.data.model.User;
+import br.com.erudio.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
