5a5169b7d1f8e8bf5063e586ced6ae5f93ddb17b
leandrocgsi <leandrocgsi@gmail.com>
2022-02-08 19:28:17 -0300

    Connecting to MySQL

--- 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/handler/CustomizedResponseEntityExceptionHandler.java
+++ 08_ConnectingToMySQL/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exception/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.erudio.exception.ExceptionResponse;
-import br.com.erudio.exception.UnsuportedMathOperationException;
+import br.com.erudio.exception.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -28,14 +28,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     }
     
     
-    @ExceptionHandler(UnsuportedMathOperationException.class)
+    @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
         ExceptionResponse exceptionResponse = 
                 new ExceptionResponse(
                         new Date(),
                         ex.getMessage(),
                         request.getDescription(false));
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
 }
