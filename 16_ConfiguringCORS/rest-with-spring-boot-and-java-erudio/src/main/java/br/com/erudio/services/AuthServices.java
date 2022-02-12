68ff402e4f9cce999a0629c74cba5ad7456567f3
leandrocgsi <leandrocgsi@gmail.com>
2022-02-12 11:57:21 -0300

    Implementing Authenticatin With JWT

---
+++ 16_ConfiguringCORS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/AuthServices.java
@@ -0,0 +1,68 @@
+package br.com.erudio.services;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.erudio.data.vo.v1.security.AccountCredentialsVO;
+import br.com.erudio.data.vo.v1.security.TokenVO;
+import br.com.erudio.repository.UserRepository;
+import br.com.erudio.security.jwt.JwtTokenProvider;
+
+@Service
+public class AuthServices {
+    
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JwtTokenProvider tokenProvider;
+    
+    @Autowired
+    UserRepository repository;
+    
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity signin(AccountCredentialsVO data) {
+        try {
+            var username = data.getUsername();
+            var password = data.getPassword();
+            
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            
+            var user = repository.findByUsername(username);
+            
+            var tokenResponse = new TokenVO();
+            
+            if (user != null) {
+                tokenResponse = tokenProvider.createAccessToken(username, user.getRoles());
+            } else {
+                throw new UsernameNotFoundException("Username " + username + " not found!");
+            }
+            return ok(tokenResponse);
+            
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username/password supplied!");
+        }
+    }
+    
+    @SuppressWarnings("rawtypes")
+    public ResponseEntity refreshToken(String username, String refreshToken) {
+        var user = repository.findByUsername(username);
+        
+        var tokenResponse = new TokenVO();
+        
+        if (user != null) {
+            tokenResponse = tokenProvider.refreshToken(refreshToken);
+        } else {
+            throw new UsernameNotFoundException("Username " + username + " not found!");
+        }
+        return ok(tokenResponse);
+    }
+}
