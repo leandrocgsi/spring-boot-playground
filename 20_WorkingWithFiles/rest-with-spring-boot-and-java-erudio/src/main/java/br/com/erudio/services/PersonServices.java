e18df6ff6144171f03fe2c495b0ed24bd3313ab2
leandrocgsi <leandrocgsi@gmail.com>
2022-02-14 16:45:25 -0300

    Working with files

---
+++ 20_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -0,0 +1,114 @@
+package br.com.erudio.services;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.erudio.controller.PersonController;
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.exception.RequiredObjectIsNullException;
+import br.com.erudio.exception.ResourceNotFoundException;
+import br.com.erudio.mapper.DozerConverter;
+import br.com.erudio.model.Person;
+import br.com.erudio.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+    
+    @Autowired
+    PersonRepository repository;
+        
+    public CollectionModel<PersonVO> findAll(Pageable pageable) {
+        var page = repository.findAll(pageable);
+        var persons = DozerConverter.parsePageOfObjects(page, PersonVO.class);
+        
+        persons
+            .stream()
+            .forEach(p -> p.add(
+                        linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()
+                    )
+            );
+        
+        Link findAllLink = linkTo(
+          methodOn(PersonController.class).findAll(pageable.getPageNumber(),
+                                                   pageable.getPageSize(),
+                                                   "asc")).withSelfRel();
+        
+        return CollectionModel.of(persons, findAllLink);
+    }    
+    
+    public CollectionModel<PersonVO>  findPersonByName(String firstName, Pageable pageable) {
+        var page = repository.findPersonByName(firstName, pageable);
+        var persons = DozerConverter.parsePageOfObjects(page, PersonVO.class);
+        persons
+        .stream()
+        .forEach(p -> p.add(
+                            linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()
+                    )
+                );
+        
+        Link findAllLink = linkTo(
+          methodOn(PersonController.class)
+              .findPersonByName(firstName,
+                    pageable.getPageNumber(),
+                    pageable.getPageSize(),
+                    "asc")
+                ).withSelfRel();
+        
+        return CollectionModel.of(persons, findAllLink);
+    }    
+    
+    public PersonVO findById(Long id) {
+
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        PersonVO personVO = DozerConverter.parseObject(entity, PersonVO.class);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return personVO;
+    }
+    
+    public PersonVO create(PersonVO person) {
+        if(person == null) throw new RequiredObjectIsNullException();
+        var entity = DozerConverter.parseObject(person, Person.class);
+        var personVO = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+        return personVO;
+    }
+
+    public PersonVO update(PersonVO person) {
+        if(person == null) throw new RequiredObjectIsNullException();
+        var entity = repository.findById(person.getKey())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        
+        entity.setFirstName(person.getFirstName());
+        entity.setLastName(person.getLastName());
+        entity.setAddress(person.getAddress());
+        entity.setGender(person.getGender());
+        
+        var personVO = DozerConverter.parseObject(repository.save(entity), PersonVO.class);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(personVO.getKey())).withSelfRel());
+        return personVO;
+    }    
+    
+    @Transactional
+    public PersonVO disablePerson(Long id) {
+        repository.disablePersons(id);          
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        PersonVO personVO = DozerConverter.parseObject(entity, PersonVO.class);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return personVO;
+    }
+
+    public void delete(Long id) {
+        Person entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        repository.delete(entity);
+    }
+}
---
+++ 20_WorkingWithFiles/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/integrationtests/controller/withjson/BookControllerJsonTest.java
@@ -0,0 +1,227 @@
+package br.com.erudio.integrationtests.controller.withjson;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.erudio.configs.TestsConfig;
+import br.com.erudio.data.vo.v1.security.AccountCredentialsVO;
+import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.erudio.integrationtests.vo.BookVO;
+import br.com.erudio.integrationtests.vo.TokenVO;
+import br.com.erudio.integrationtests.vo.wrappers.WrapperBookVO;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder(OrderAnnotation.class)
+public class BookControllerJsonTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+
+    private static BookVO book;
+
+    @BeforeAll
+    public static void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        
+        book = new BookVO();
+    }
+    
+    @Test
+    @Order(1)
+    public void authorization() {
+        AccountCredentialsVO user = new AccountCredentialsVO();
+        user.setUsername("leandro");
+        user.setPassword("admin123");
+
+        var token =
+                given()
+                    .basePath("/auth/signin")
+                    .port(TestsConfig.SERVER_PORT)
+                    .contentType(TestsConfig.CONTENT_TYPE_JSON)
+                    .body(user)
+                    .when()
+                        .post()
+                    .then()
+                        .statusCode(200)
+                    .extract()
+                    .body()
+                        .as(TokenVO.class)
+                    .getAccessToken();
+
+            specification =
+                new RequestSpecBuilder()
+                    .addHeader(TestsConfig.HEADER_PARAM_AUTHORIZATION, "Bearer " + token)
+                    .setBasePath("/api/book/v1")
+                    .setPort(TestsConfig.SERVER_PORT)
+                    .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                    .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+                    .build();
+    }
+      
+    @Test
+    @Order(2)
+    public void testCreate() throws JsonMappingException, JsonProcessingException {
+        
+        mockBook();
+
+        var content = given().spec(specification)
+                .contentType(TestsConfig.CONTENT_TYPE_JSON)
+                    .body(book)
+                    .when()
+                    .post()
+                .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                            .asString();
+        
+        book = objectMapper.readValue(content, BookVO.class);
+        
+        assertNotNull(book.getId());
+        assertNotNull(book.getTitle());
+        assertNotNull(book.getAuthor());
+        assertNotNull(book.getPrice());
+        assertTrue(book.getId() > 0);
+        assertEquals("Docker Deep Dive", book.getTitle());
+        assertEquals("Nigel Poulton", book.getAuthor());
+        assertEquals(55.99, book.getPrice());
+    }
+      
+    @Test
+    @Order(3)
+    public void testUpdate() throws JsonMappingException, JsonProcessingException {
+        
+        book.setTitle("Docker Deep Dive - Updated");
+
+        var content = given().spec(specification)
+                .contentType(TestsConfig.CONTENT_TYPE_JSON)
+                    .body(book)
+                    .when()
+                    .put()
+                .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                            .asString();
+        
+        BookVO bookUpdated = objectMapper.readValue(content, BookVO.class);
+        
+        assertNotNull(bookUpdated.getId());
+        assertNotNull(bookUpdated.getTitle());
+        assertNotNull(bookUpdated.getAuthor());
+        assertNotNull(bookUpdated.getPrice());
+        assertEquals(bookUpdated.getId(), book.getId());
+        assertEquals("Docker Deep Dive - Updated", bookUpdated.getTitle());
+        assertEquals("Nigel Poulton", bookUpdated.getAuthor());
+        assertEquals(55.99, bookUpdated.getPrice());
+    }
+
+    @Test
+    @Order(4)
+    public void testFindById() throws JsonMappingException, JsonProcessingException {
+        var content = given().spec(specification)
+                .contentType(TestsConfig.CONTENT_TYPE_JSON)
+                    .pathParam("id", book.getId())
+                    .when()
+                    .get("{id}")
+                .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                            .asString();
+        
+        BookVO foundBook = objectMapper.readValue(content, BookVO.class);
+        
+        assertNotNull(foundBook.getId());
+        assertNotNull(foundBook.getTitle());
+        assertNotNull(foundBook.getAuthor());
+        assertNotNull(foundBook.getPrice());
+        assertEquals(foundBook.getId(), book.getId());
+        assertEquals("Docker Deep Dive - Updated", foundBook.getTitle());
+        assertEquals("Nigel Poulton", foundBook.getAuthor());
+        assertEquals(55.99, foundBook.getPrice());
+    }
+    
+    @Test
+    @Order(5)
+    public void testDelete() {
+        given().spec(specification)
+                .contentType(TestsConfig.CONTENT_TYPE_JSON)
+                    .pathParam("id", book.getId())
+                    .when()
+                    .delete("{id}")
+                .then()
+                    .statusCode(204);
+    }
+    
+    @Test
+    @Order(6)
+    public void testFindAll() throws JsonMappingException, JsonProcessingException {
+
+        var json = given().spec(specification)
+                .contentType(TestsConfig.CONTENT_TYPE_JSON)
+                    .queryParams("page", 0 , "limit", 5, "direction", "asc")
+                    .when()
+                    .get()
+                    .then()
+                    .statusCode(200)
+                        .extract()
+                        .body()
+                        .asString();
+        
+        WrapperBookVO wrapper = objectMapper.readValue(json, WrapperBookVO.class);
+        
+        var books = wrapper.getEmbedded().getBooks();
+    
+        BookVO foundBookOne = books.get(0);
+        
+        assertNotNull(foundBookOne.getId());
+        assertNotNull(foundBookOne.getTitle());
+        assertNotNull(foundBookOne.getAuthor());
+        assertNotNull(foundBookOne.getPrice());
+        assertTrue(foundBookOne.getId() > 0);
+        assertEquals("Big Data: como extrair volume, variedade, velocidade e valor da avalanche de informação cotidiana", foundBookOne.getTitle());
+        assertEquals("Viktor Mayer-Schonberger e Kenneth Kukier", foundBookOne.getAuthor());
+        assertEquals(54.00, foundBookOne.getPrice());
+        
+        BookVO foundBookFive = books.get(4);
+        
+        assertNotNull(foundBookFive.getId());
+        assertNotNull(foundBookFive.getTitle());
+        assertNotNull(foundBookFive.getAuthor());
+        assertNotNull(foundBookFive.getPrice());
+        assertTrue(foundBookFive.getId() > 0);
+        assertEquals("Domain Driven Design", foundBookFive.getTitle());
+        assertEquals("Eric Evans", foundBookFive.getAuthor());
+        assertEquals(92.0, foundBookFive.getPrice());
+    }
+     
+    private void mockBook() {
+        book.setTitle("Docker Deep Dive");
+        book.setAuthor("Nigel Poulton");
+        book.setPrice(Double.valueOf(55.99));
+        book.setLaunchDate(new Date());
+    }    
+}
