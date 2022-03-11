9a99e6fd637f2c1e6a7f4df46eec2ba813e041bc
leandrocgsi <leandrocgsi@gmail.com>
2022-03-11 09:33:21 -0300

    Migrating to new Versions of Frameworks

--- 99_MigratingToNewVersions/src/main/java/br/com/erudio/Startup.java
+++ 99_MigratingToNewVersions/src/main/java/br/com/erudio/Startup.java
@@ -1,8 +1,14 @@
 package br.com.erudio;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 import br.com.erudio.config.FileStorageConfig;
 
@@ -15,7 +21,7 @@ public class Startup {
     public static void main(String[] args) {
         SpringApplication.run(Startup.class, args);
         
-        /*Map<String, PasswordEncoder> encoders = new HashMap<>();
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
         encoders.put("pbkdf2", new Pbkdf2PasswordEncoder());
         DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
         passwordEncoder.setDefaultPasswordEncoderForMatches(new Pbkdf2PasswordEncoder());
@@ -23,6 +29,6 @@ public class Startup {
         
         //DelegatingPasswordEncoder passwordEncoder = (DelegatingPasswordEncoder) PasswordEncoderFactories.createDelegatingPasswordEncoder();
         String result = passwordEncoder.encode("admin234");
-        System.out.println("My hash " + result);*/
+        System.out.println("My hash " + result);
     }
 }
