3fea9972950bd5e7a350d1394f33091e4178958e
leandrocgsi <leandrocgsi@gmail.com>
2023-01-08 16:02:07 -0300

    Testing Generation of HATEOAS Links with Mockito

--- 14_HATEOAS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 14_HATEOAS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -24,7 +24,11 @@ public class PersonServices {
 
 		logger.info("Finding all people!");
 
-		return ErudioMapper.parseListObjects(repository.findAll(), PersonVO.class);
+		var persons = ErudioMapper.parseListObjects(repository.findAll(), PersonVO.class);
+		persons
+			.stream()
+			.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		return persons;
 	}
 
 	public PersonVO findById(Long id) {
@@ -33,22 +37,29 @@ public class PersonServices {
 		
 		var entity = repository.findById(id)
 			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
-		return ErudioMapper.parseObject(entity, PersonVO.class);
+		var vo = ErudioMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
 	}
 	
 	public PersonVO create(PersonVO person) {
 
+		if (person == null) throw new RequiredObjectIsNullException();
+		
 		logger.info("Creating one person!");
 		var entity = ErudioMapper.parseObject(person, Person.class);
 		var vo =  ErudioMapper.parseObject(repository.save(entity), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
 	
 	public PersonVO update(PersonVO person) {
 		
+		if (person == null) throw new RequiredObjectIsNullException();
+		
 		logger.info("Updating one person!");
 		
-		var entity = repository.findById(person.getId())
+		var entity = repository.findById(person.getKey())
 			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
 		entity.setFirstName(person.getFirstName());
@@ -57,6 +68,7 @@ public class PersonServices {
 		entity.setGender(person.getGender());
 		
 		var vo =  ErudioMapper.parseObject(repository.save(entity), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
 	
