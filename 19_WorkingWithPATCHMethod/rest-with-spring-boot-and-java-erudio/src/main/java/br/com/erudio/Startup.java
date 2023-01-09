97ac90a31695bb759a3e96b6c7d7181e4b5d4ed8
leandrocgsi <leandrocgsi@gmail.com>
2023-01-09 15:28:30 -0300

    Working with PATCH Method

---
+++ 19_WorkingWithPATCHMethod/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/Startup.java
@@ -0,0 +1,30 @@
+package br.com.erudio;
+
+//import java.util.HashMap;
+//import java.util.Map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+@SpringBootApplication
+public class Startup {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Startup.class, args);
+        
+        /**
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        Pbkdf2PasswordEncoder encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+        encoders.put("pbkdf2", encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(encoder);
+        
+        String result = passwordEncoder.encode("admin123");
+        String result2 = passwordEncoder.encode("admin234");
+        System.out.println("My hash " + result);
+        */
+    }
+}
