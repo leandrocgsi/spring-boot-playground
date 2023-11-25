55b37b0bd091668d26ea83d3f746e76726d05a72
leandrocgsi <leandrocgsi@gmail.com>
2023-11-25 14:57:50 -0300

    Implementing VO pattern with Dozer and Orika

--- 09_WorkingWithVOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/mapper/DozerMapper.java
+++ 09_WorkingWithVOPattern/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/mapper/DozerMapper.java
@@ -3,11 +3,16 @@ package br.com.erudio.mapper;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.modelmapper.ModelMapper;
+// import org.modelmapper.ModelMapper;
 
-public class ErudioMapper {
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+public class DozerMapper {
 	
-	private static final ModelMapper mapper = new ModelMapper();
+	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	// private static ModelMapper mapper = new ModelMapper();
 	
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
 		return mapper.map(origin, destination);
