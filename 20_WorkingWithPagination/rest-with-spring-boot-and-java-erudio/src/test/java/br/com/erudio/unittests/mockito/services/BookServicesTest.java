3849001f6e1b2d436c4cbbda5727751c85e6e1e6
leandrocgsi <leandrocgsi@gmail.com>
2023-11-25 15:02:39 -0300

    Implementing Pagination Search

--- 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mockito/services/BookServicesTest.java
+++ 20_WorkingWithPagination/rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/unittests/mockito/services/BookServicesTest.java
@@ -52,7 +52,7 @@ class BookServicesTest {
 		
 		var result = service.findById(1L);
 		assertNotNull(result);
-		assertNotNull(result.getId());
+		assertNotNull(result.getKey());
 		assertNotNull(result.getLinks());
 		
 		assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
@@ -71,14 +71,14 @@ class BookServicesTest {
 		persisted.setId(1L);
 		
 		BookVO vo = input.mockVO(1);
-		vo.setId(1L);
+		vo.setKey(1L);
 		
 		when(repository.save(entity)).thenReturn(persisted);
 		
 		var result = service.create(vo);
 		
 		assertNotNull(result);
-		assertNotNull(result.getId());
+		assertNotNull(result.getKey());
 		assertNotNull(result.getLinks());
 		
 		assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
@@ -109,7 +109,7 @@ class BookServicesTest {
 		persisted.setId(1L);
 		
 		BookVO vo = input.mockVO(1);
-		vo.setId(1L);
+		vo.setKey(1L);
 		
 
 		when(repository.findById(1L)).thenReturn(Optional.of(entity));
@@ -118,7 +118,7 @@ class BookServicesTest {
 		var result = service.update(vo);
 		
 		assertNotNull(result);
-		assertNotNull(result.getId());
+		assertNotNull(result.getKey());
 		assertNotNull(result.getLinks());
 		
 		assertTrue(result.toString().contains("links: [</api/book/v1/1>;rel=\"self\"]"));
