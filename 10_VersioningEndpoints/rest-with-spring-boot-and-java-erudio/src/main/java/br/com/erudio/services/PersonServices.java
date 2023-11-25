c5e1accd2dfe77772061bbeeaaad18393ee8ebba
leandrocgsi <leandrocgsi@gmail.com>
2023-11-25 14:58:06 -0300

    Versioning our API

--- 10_VersioningEndpoints/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 10_VersioningEndpoints/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.exceptions.ResourceNotFoundException;
-import br.com.erudio.mapper.ErudioMapper;
+import br.com.erudio.mapper.DozerMapper;
 import br.com.erudio.mapper.custom.PersonMapper;
 import br.com.erudio.model.Person;
 import br.com.erudio.repositories.PersonRepository;
@@ -21,7 +21,7 @@ public class PersonServices {
 	
 	@Autowired
 	PersonRepository repository;
-
+	
 	@Autowired
 	PersonMapper mapper;
 
@@ -29,7 +29,7 @@ public class PersonServices {
 
 		logger.info("Finding all people!");
 
-		return ErudioMapper.parseListObjects(repository.findAll(), PersonVO.class);
+		return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
 	}
 
 	public PersonVO findById(Long id) {
@@ -38,14 +38,14 @@ public class PersonServices {
 		
 		var entity = repository.findById(id)
 			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
-		return ErudioMapper.parseObject(entity, PersonVO.class);
+		return DozerMapper.parseObject(entity, PersonVO.class);
 	}
 	
 	public PersonVO create(PersonVO person) {
 
 		logger.info("Creating one person!");
-		var entity = ErudioMapper.parseObject(person, Person.class);
-		var vo =  ErudioMapper.parseObject(repository.save(entity), PersonVO.class);
+		var entity = DozerMapper.parseObject(person, Person.class);
+		var vo =  DozerMapper.parseObject(repository.save(entity), PersonVO.class);
 		return vo;
 	}
 	
@@ -69,7 +69,7 @@ public class PersonServices {
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
 		
-		var vo =  ErudioMapper.parseObject(repository.save(entity), PersonVO.class);
+		var vo =  DozerMapper.parseObject(repository.save(entity), PersonVO.class);
 		return vo;
 	}
 	
