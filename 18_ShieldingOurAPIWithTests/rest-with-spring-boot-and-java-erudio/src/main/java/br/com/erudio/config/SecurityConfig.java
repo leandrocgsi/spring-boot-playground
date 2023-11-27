15fd4f45581c8719fe95f1c85b0851fe589e5c38
leandrocgsi <leandrocgsi@gmail.com>
2023-11-27 00:28:04 -0300

    Shielding our API with Rest Assured, Testcontainers and JUnit 5

--- 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/config/SecurityConfig.java
@@ -7,54 +7,57 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import br.com.erudio.security.jwt.JwtConfigurer;
+import br.com.erudio.security.jwt.JwtTokenFilter;
 import br.com.erudio.security.jwt.JwtTokenProvider;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-	@Autowired
-	private JwtTokenProvider tokenProvider;
-	
-	@Bean
-	PasswordEncoder passwordEncoder() {
-		Map<String, PasswordEncoder> encoders = new HashMap<>();
-				
-		Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
-		encoders.put("pbkdf2", pbkdf2Encoder);
-		DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
-		passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
-		return passwordEncoder;
-	}
-	
+    @Autowired
+    private JwtTokenProvider tokenProvider;
+
     @Bean
-    AuthenticationManager authenticationManagerBean(
-    		AuthenticationConfiguration authenticationConfiguration)
+    PasswordEncoder passwordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+
+        Pbkdf2PasswordEncoder pbkdf2Encoder = new Pbkdf2PasswordEncoder("", 8, 185000,
+                SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
+        encoders.put("pbkdf2", pbkdf2Encoder);
+        DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
+        return passwordEncoder;
+    }
+
+    @Bean
+    AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        
+        JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+        
+        //@formatter:off
         http
-            //.httpBasic(basic -> basic.disable())
-            .httpBasic(HttpBasicConfigurer::disable)
+            .httpBasic(basic -> basic.disable())
             .csrf(csrf -> csrf.disable())
+            // .httpBasic(HttpBasicConfigurer::disable)
             // .csrf(AbstractHttpConfigurer::disable)
+            .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
             .sessionManagement(
         		session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(
@@ -68,8 +71,17 @@ public class SecurityConfig {
                     .requestMatchers("/api/**").authenticated()
                     .requestMatchers("/users").denyAll()
             )
-            .cors(cors -> {})
-            .apply(new JwtConfigurer(tokenProvider));
+            .cors(cors -> {});
+            //.apply()
+            // .apply(jwtConfigurer());
         return http.build();
+        //@formatter:on
     }
+    
+    /*
+    @Bean
+    JwtConfigurer jwtConfigurer() {
+        return new JwtConfigurer(tokenProvider);
+    }
+    */
 }
--- 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
@@ -1,13 +1,19 @@
+/*
 package br.com.erudio.security.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class JwtConfigurer extends
-		SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{
+@Configuration
+public class JwtConfigurer {
+    // extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{
+    // https://cursos.alura.com.br/forum/topico-websecurityconfigureradapter-deprecated-222772
 
 	@Autowired
 	private JwtTokenProvider tokenProvider;
@@ -16,9 +22,18 @@ public class JwtConfigurer extends
 		this.tokenProvider = tokenProvider;
 	}
 
+	/*
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
 		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
 	}
-}
+	
+	
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}*/
