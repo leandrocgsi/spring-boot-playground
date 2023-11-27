e8c869b1771d9d088aa7c1bbd97f1dc3a4f9caa4
leandrocgsi <leandrocgsi@gmail.com>
2023-11-27 17:28:14 -0300

    Shielding our API with Rest Assured, Testcontainers and JUnit 5

--- 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
+++ 17_AuthenticationWithJWT/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
@@ -6,8 +6,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class JwtConfigurer extends
-		SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{
+@Configuration
+public class JwtConfigurer {
+    // extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{
+    // https://cursos.alura.com.br/forum/topico-websecurityconfigureradapter-deprecated-222772
 
 	@Autowired
 	private JwtTokenProvider tokenProvider;
@@ -16,9 +18,18 @@ public class JwtConfigurer extends
 		this.tokenProvider = tokenProvider;
 	}
 
+	/*
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
 		http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
 	}
+	*/
+	
+    @Bean
+    SecurityFilterChain addFilterBefore(HttpSecurity http) throws Exception {
+        JwtTokenFilter customFilter = new JwtTokenFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
--- 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
+++ 18_ShieldingOurAPIWithTests/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/security/jwt/JwtConfigurer.java
@@ -2,14 +2,10 @@ package br.com.erudio.security.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
 public class JwtConfigurer {
     // extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity>{
     // https://cursos.alura.com.br/forum/topico-websecurityconfigureradapter-deprecated-222772
