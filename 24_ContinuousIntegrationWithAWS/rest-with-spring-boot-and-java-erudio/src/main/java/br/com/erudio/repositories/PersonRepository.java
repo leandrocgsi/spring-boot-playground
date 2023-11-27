5a17f449e0b89056805a43d8fb66f9a39f9be9c2
leandrocgsi <leandrocgsi@gmail.com>
2023-11-27 19:14:34 -0300

    Continuous Integration with Github, Github Actions, Amazon ECR and ECS

--- 24_ContinuousIntegrationWithAWS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/PersonRepository.java
+++ 24_ContinuousIntegrationWithAWS/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/repositories/PersonRepository.java
@@ -6,11 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import br.com.erudio.model.Person;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 	
 	@Modifying
