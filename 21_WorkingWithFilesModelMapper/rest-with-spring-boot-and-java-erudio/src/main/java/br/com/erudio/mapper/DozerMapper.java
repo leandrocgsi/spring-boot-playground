bb084ead3a90cbbb528f955add30699e8aad0e3e
leandrocgsi <leandrocgsi@gmail.com>
2023-12-03 18:41:44 -0300

    Implementing Upload and Download Files

--- 21_WorkingWithFilesModelMapper/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/mapper/DozerMapper.java
+++ 21_WorkingWithFilesModelMapper/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/mapper/DozerMapper.java
@@ -4,41 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-import br.com.erudio.data.vo.v1.BookVO;
-import br.com.erudio.data.vo.v1.PersonVO;
-import br.com.erudio.model.Book;
-import br.com.erudio.model.Person;
-
+@Component
 public class DozerMapper {
     
-    private static ModelMapper mapper = new ModelMapper();
+    @Autowired
+    ModelMapper mapper;
     
-    static {
-        mapper.createTypeMap(
-                Person.class,
-                PersonVO.class)
-            .addMapping(Person::getId, PersonVO::setKey);
-        mapper.createTypeMap(
-                PersonVO.class,
-                Person.class)
-            .addMapping(PersonVO::getKey, Person::setId);
-        
-        mapper.createTypeMap(
-                Book.class,
-                BookVO.class)
-        .addMapping(Book::getId, BookVO::setKey);
-        mapper.createTypeMap(
-                BookVO.class,
-                Book.class)
-        .addMapping(BookVO::getKey, Book::setId);
-    }
-    
-    public static <O, D> D parseObject(O origin, Class<D> destination) {
+    public <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
     }
     
-    public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+    public <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
         List<D> destinationObjects = new ArrayList<D>();
         for (O o : origin) {
             destinationObjects.add(mapper.map(o, destination));
